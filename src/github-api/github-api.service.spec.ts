04613a9338c170794808fa232972e0663aa38d52
Rodrigo Fernandes <rodrigo.melero@compasso.com.br>
2021-08-26 12:00:38 -0300

    Criando um método para buscar o usuário do github

--- src/github-api/github-api.service.spec.ts
+++ src/github-api/github-api.service.spec.ts
@@ -1,5 +1,4 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { checkServerIdentity } from 'tls';
 import { GithubApiHttpClient } from './github-api-http-client';
 import { GithubApiService } from './github-api.service';
 
