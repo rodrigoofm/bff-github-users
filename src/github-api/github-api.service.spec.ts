cf549712e3f49abfe09579588f60fc2d272e0700
Rodrigo Fernandes <rodrigo.melero@compasso.com.br>
2021-09-03 15:06:36 -0300

    Ajustando os testes de repositories

--- src/github-api/github-api.service.spec.ts
+++ src/github-api/github-api.service.spec.ts
@@ -34,8 +34,8 @@ describe('GithubApiService', () => {
 
   it('should get repository by username', async () => {
     const username = 'rodrigo';
-    const user = await githubApiService.findRepos(username);
-    expect(user).toBeDefined();
-    expect(user.login).toBe(username);
+    const repos = await githubApiService.findRepos(username);
+    expect(repos).toBeDefined();
+    expect(repos[0].login).toBe(username);
   });
 });
